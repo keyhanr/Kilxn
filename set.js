cf849e7a976e05d87e6ae6b18045ec45ac6aef6c
keyhanr <keyhanrez@hotmail.com>
2015-11-12 15:54:32 -0500

    Made the enter key functional

--- set.js
+++ set.js
@@ -4,20 +4,21 @@ dBox.id = "setBox";
 // Give most defaults values so as to not inherit from document style
 var buttonStyle = "font-family: Segoe UI, Lucida Grande, Tahoma, " +
     "sans-serif; font-size: 12px; padding:1px 3px; margin: 2px; background-" +
-    "color:#eee; color:#000; border-radius:3px; background-size:auto; border: 1px "+
+    "color:#fff; color:#000; border-radius:3px; background-size:auto; border: 1px "+
     "#bbe solid; box-shadow:#ccc 0px -1px 1px inset; line-height: 15px;" + 
-    "box-sizing: initial;";
+    "box-sizing: border-box; outline:none; onkeypress: 'console.log('h');";
 
 var inputStyle = "font-family: Segoe UI, Lucida Grande, Tahoma, " +
-    "sans-serif; font-size: 13px; padding:2px; margin: 2px; border:1px #bbb solid" +
-    "box-sizing: initial";
+    "sans-serif; font-size: 13px; padding:2px; margin: 2px; border:1px #bbb solid;" +
+    "line-height: 15px";
 
 dBox.setAttribute("style", "width:150px; height:108px; position:fixed; left:50%; "+
-    "top:50%; background:#DDD; font-family: Segoe UI, Lucida Grande, Tahoma, " +
-    "sans-serif; font-size: 16px; border:1px solid #555; z-index:999; margin" +
+    "top:50%; background:#eee; font-family: Segoe UI, Lucida Grande, Tahoma, " +
+    "sans-serif; font-size: 16px; border:1px solid #ccc; z-index:999; margin" +
     "-left:-78px; margin-top:-75px; padding:3px 6px 0px; line-height:150%;" + 
     "border-radius: 3px; opacity: 100%; text-align:center; box-shadow:" +
-    "rgba(0, 0, 0, 0.2) 1px 1px 1px 1px; box-sizing: initial");
+    "rgba(0, 0, 0, 0.1) 1px 1px 6px 2px; box-sizing: initial; text-tranform:" +
+    "none; box-sizing: content-box; ");
 
 dBox.innerHTML += "<center>Add to Kilxn</center>";
 
@@ -25,8 +26,8 @@ dBox.innerHTML += "<center>Add to Kilxn</center>";
 var desBox = document.createElement("input");
 desBox.setAttribute('type', 'text');
 desBox.setAttribute('placeholder', "Description");
-desBox.setAttribute('id', 'des');
 desBox.setAttribute('style', inputStyle);
+desBox.id = 'klxnDescBox';
 
 dBox.appendChild(desBox);
 
@@ -34,37 +35,19 @@ dBox.appendChild(desBox);
 var tagBox = document.createElement("input");
 tagBox.setAttribute('type', 'text');
 tagBox.setAttribute('placeholder', 'Tags');
-tagBox.setAttribute('id', 'tags');
 tagBox.setAttribute('style', inputStyle);
+tagBox.id = 'klxnTagBox';
 
 dBox.appendChild(tagBox);
 
 dBox.innerHTML += '<br>';
-
-//var pathMenu = document.createElement("select");
-/*var option = 1;
-for (String dir: dirs) {
-    var pathOption = document.createElement("option");
-    pathOption.value = dir + option;
-    option = option + 1;
-    pathMenu.appendChild(pathOption);
-}*/
-// var klxnsOption = document.createElement("option");
-// klxnsOption.text = "Klxns";
-// pathMenu.add(klxnsOption);
-/*var addNewOption = document.createElement("option");
-addNewOption.text = "Make new klxn...";
-addNewOption.onClick = function () {
-    dBox.insertBefore(newPath, )
-}
-pathMenu.add(addNewOption);
-dBox.appendChild(pathMenu);*/
 
 var submitBut = document.createElement("input");
 submitBut.type = "button";
 submitBut.value = "Add";
 submitBut.onclick = addToKlxn;
 submitBut.setAttribute('style', buttonStyle);
+submitBut.id = "submitButtonKlxns";
 dBox.appendChild(submitBut);
 
 var cancelBut =  document.createElement("input");
@@ -76,10 +59,26 @@ dBox.appendChild(cancelBut);
 
 document.body.appendChild(dBox);
 
+document.getElementById("klxnDescBox").focus();
+
+// Ideally I would have a method to check for the enter key, but for some reason
+// only this works, so I have to write it for each field
+document.getElementById("klxnDescBox").addEventListener('keydown', function (e) {
+    if (e.keyCode == 13) {
+        addToKlxn();
+    }
+});
+
+document.getElementById("klxnDescBox").addEventListener('keydown', function (e) {
+    if (e.keyCode == 13) {
+        addToKlxn();
+    }
+});
+
 function addToKlxn () {
     var imag = {src: imgUrl, desc: "", imgTags: ""};
-    imag.desc = document.getElementById('des').value;
-    imag.imgTags = document.getElementById('tags').value;
+    imag.desc = document.getElementById('klxnDescBox').value;
+    imag.imgTags = tagBox.value;
     closeBox();
     chrome.storage.local.get({klxn: []}, function (result) {
     // the input argument is ALWAYS an object containing the queried keys
