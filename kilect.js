f9adbcc9d9dfcbd9bd9f71eb39b07a047f172329
keyhanr <keyhanrez@hotmail.com>
2015-11-12 15:55:49 -0500

    Injects script instead
    The only way to add a div

--- kilect.js
+++ kilect.js
@@ -14,20 +14,3 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
 //edit
 //style
 // directories
-
-
-// // by passing an object you can define default values e.g.: []
-// chrome.storage.local.get({userKeyIds: []}, function (result) {
-//     // the input argument is ALWAYS an object containing the queried keys
-//     // so we select the key we need
-//     var userKeyIds = result.userKeyIds;
-//     userKeyIds.push({keyPairId: keyPairId, HasBeenUploadedYet: false});
-//     // set the new array value to the same key
-//     chrome.storage.local.set({userKeyIds: userKeyIds}, function () {
-//         // you can use strings instead of objects
-//         // if you don't  want to define default values
-//         chrome.storage.local.get('userKeyIds', function (result) {
-//             console.log(result.userKeyIds)
-//         });
-//     });
-// });
